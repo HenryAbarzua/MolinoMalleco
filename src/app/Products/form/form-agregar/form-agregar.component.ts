7ffd1bf447d1f7518696637e48aea53730664113
HenryAbarzua <54822769+HenryAbarzua@users.noreply.github.com>
2020-12-22 01:10:09 -0300

    Fail

--- src/app/Products/form/form-agregar/form-agregar.component.ts
+++ src/app/Products/form/form-agregar/form-agregar.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from "../../../service/products.service";
 import {MAT_DIALOG_DATA,MatDialogRef,MatDialog,} from "@angular/material/dialog";
-import {FlashMessagesService} from 'angular2-flash-messages';
-
+ 
 @Component({
   selector: 'app-form-agregar',
   templateUrl: './form-agregar.component.html',
@@ -10,16 +9,15 @@ import {FlashMessagesService} from 'angular2-flash-messages';
 })
 export class FormAgregarComponent implements OnInit {
   cantidad: number = 0;
-
+ 
   constructor(
     public products: ProductsService,
-    public flashMensaje : FlashMessagesService,
     private dialogRef: MatDialogRef<FormAgregarComponent>
   ) { }
-
+ 
   ngOnInit(): void {
   }
-
+ 
   onSaveForm() {
     if (this.products.selected.id == null) {
       let newProducts = {
@@ -27,19 +25,12 @@ export class FormAgregarComponent implements OnInit {
         cantidad: this.products.selected.cantidad,
       };
       this.products.addProducts(newProducts);
-    }else if(this.cantidad < 900)
-    {
-      this.flashMensaje.show('cantidad erronea',{cssClass:'alert-success',timeout:4000});
-    } 
-    
-    else {
+    } else {
       let num = parseInt(this.products.selected.cantidad.toString()) + this.cantidad
       this.products.selected.cantidad = num
       this.products.editProducts(this.products.selected);
-
     }
     this.close();
-
   }
   close(): void {
     this.dialogRef.close();
--- src/app/components/laboratorio/laboratorio.component.ts
+++ src/app/components/laboratorio/laboratorio.component.ts
@@ -11,5 +11,6 @@ export class LaboratorioComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
 
 }
